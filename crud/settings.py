3c0b2ec36ed25b19e5acf7b4fe30c15d2c995329
himalAcharya <himalacharyayt@gmail.com>
2025-02-06 08:23:18 +0545

    re

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9o@4e-pcczm@gmjr(cjikbgpbz2l-=_f$0ts1i69jkqy4+#6&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-vercel-project-name.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['crud.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
